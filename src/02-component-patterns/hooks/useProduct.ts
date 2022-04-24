041d99acb4997983c328864ae5252205ae6e8801
Martin Vera <74189587+MartinIsProgramming@users.noreply.github.com>
2022-04-24 13:37:21 -0300

    Control props alternative implemented

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -10,13 +10,7 @@ interface Props {
 const useProduct = ({ onChange, product, value = 0 }: Props) => {
   const [counter, setCounter] = useState<number>(value);
 
-  const isControlled = useRef(!!onChange);
-
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
-
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
