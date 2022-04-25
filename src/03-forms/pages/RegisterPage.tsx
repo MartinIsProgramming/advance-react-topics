5e84af632f75b500ce04789f6e9ea853db0cb4dd
Martin Vera <74189587+MartinIsProgramming@users.noreply.github.com>
2022-04-25 09:27:18 -0300

    how react handle forms by default

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,15 +1,60 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import '../styles/styles.css';
 
 const RegisterPage = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const onHandleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setFormState(prev => ({
+      ...prev,
+      [ev.target.name]: ev.target.value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(formState);
+  };
+
   return (
     <div>
       <h1>RegisterPage</h1>
 
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Repeat password" />
+      <form noValidate onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={onHandleChange}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={onHandleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={onHandleChange}
+        />
+        <label htmlFor="password2">Repeat Password</label>
+        <input
+          name="password2"
+          type="password"
+          placeholder="Repeat password"
+          onChange={onHandleChange}
+        />
         <button type="submit">Create</button>
       </form>
     </div>
