b2fa70a172964787b8b7fd0d9acc70a262261c81
Martin Vera <74189587+MartinIsProgramming@users.noreply.github.com>
2022-04-25 10:18:57 -0300

    Formik components

--- src/03-forms/pages/FormikPage.tsx
+++ src/03-forms/pages/FormikPage.tsx
@@ -1,62 +1,76 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import '../styles/styles.css';
 
 const FormikPage = () => {
-  const [formState, setFormState] = useState({
-    name: '',
+  const initialValues = {
+    firstName: '',
+    lastName: '',
     email: '',
-    password: '',
-    password2: '',
+    jobType: '',
+    terms: false,
+  };
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string()
+      .max(15, 'Only fifteen characters allowed')
+      .required(),
+    lastName: Yup.string()
+      .max(15, 'Only fifteen characters allowed')
+      .required(),
+    email: Yup.string().email('email format is not valid').required(),
+    jobType: Yup.string()
+      .required('Please select a job')
+      .notOneOf(['it-junior'], 'This option is not available at this moment'),
+    terms: Yup.bool().oneOf(
+      [true],
+      'You must accept the terms and conditions to continue'
+    ),
   });
-
-  const onHandleChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    setFormState(prev => ({
-      ...prev,
-      [ev.target.name]: ev.target.value,
-    }));
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    console.log(formState);
-  };
 
   return (
     <div>
       <h1>FormikPage</h1>
 
-      <form noValidate onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          name="name"
-          type="text"
-          placeholder="Name"
-          onChange={onHandleChange}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          onChange={onHandleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          onChange={onHandleChange}
-        />
-        <label htmlFor="password2">Repeat Password</label>
-        <input
-          name="password2"
-          type="password"
-          placeholder="Repeat password"
-          onChange={onHandleChange}
-        />
-        <button type="submit">Create</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={values => {
+          console.log(values);
+        }}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" type="text" placeholder="First name" />
+          <ErrorMessage name="firstName" component="span" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" type="text" placeholder="Last Name" />
+          <ErrorMessage name="lastName" component="span" />
+
+          <label htmlFor="email">Email</label>
+          <Field name="email" type="email" placeholder="email" />
+          <ErrorMessage name="email" component="span" />
+
+          <label htmlFor="jobType">Job Type</label>
+          <Field name="jobType" as="select">
+            <option value="">Pick a job</option>
+            <option value="developer">Developer</option>
+            <option value="designer">Designer</option>
+            <option value="it-senior">IT-Senior</option>
+            <option value="it-junior">IT-Junior</option>
+          </Field>
+          <ErrorMessage name="jobType" component="span" />
+
+          <label>
+            <Field name="terms" type="checkbox" />
+            Terms and conditions
+          </label>
+          <ErrorMessage name="terms" component="span" />
+
+          <button type="submit">Create</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
